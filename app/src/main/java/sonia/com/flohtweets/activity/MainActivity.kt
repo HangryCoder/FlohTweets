78ee4e7afac710b6dc66e006cb5b36b743c922e0
= <=>
2018-11-17 17:24:12 +0530

    Commit 1.2
    Working on TweetsAdapter. DONE - Hardcoded

--- app/src/main/java/sonia/com/flohtweets/activity/MainActivity.kt
+++ app/src/main/java/sonia/com/flohtweets/activity/MainActivity.kt
@@ -2,12 +2,65 @@ package sonia.com.flohtweets.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
 import sonia.com.flohtweets.R
+import sonia.com.flohtweets.adapter.TweetsAdapter
+import sonia.com.flohtweets.model.Tweets
+import sonia.com.flohtweets.utils.VerticalItemDecoration
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var layoutManager: LinearLayoutManager
+    private var tweetsList: ArrayList<Tweets> = ArrayList()
+    private lateinit var tweetsAdapter: TweetsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        fetchTweets()
+
+        tweetsAdapter = TweetsAdapter(
+            context = this@MainActivity,
+            tweetsList = tweetsList
+        )
+        layoutManager = LinearLayoutManager(this@MainActivity)
+        tweetsRecyclerView.layoutManager = layoutManager
+        tweetsRecyclerView.addItemDecoration(VerticalItemDecoration(10))
+
+        tweetsRecyclerView.adapter = tweetsAdapter
+    }
+
+    private fun fetchTweets() {
+        var tweets = Tweets(
+            tweetId = 1, tweetUsername = "Sonia Wadji",
+            tweetUserProfile = "dfvfv", tweetMessage = ""
+        )
+        tweetsList.add(tweets)
+
+        tweets = Tweets(
+            tweetId = 2, tweetUsername = "Stephen D'Souza",
+            tweetUserProfile = "dfvfv", tweetMessage = ""
+        )
+        tweetsList.add(tweets)
+
+        tweets = Tweets(
+            tweetId = 3, tweetUsername = "Krupa Bhat",
+            tweetUserProfile = "dfvfv", tweetMessage = ""
+        )
+        tweetsList.add(tweets)
+
+        tweets = Tweets(
+            tweetId = 4, tweetUsername = "Suhail Shaikh",
+            tweetUserProfile = "dfvfv", tweetMessage = ""
+        )
+        tweetsList.add(tweets)
+
+        tweets = Tweets(
+            tweetId = 1, tweetUsername = "Shilpa Wadji",
+            tweetUserProfile = "dfvfv", tweetMessage = ""
+        )
+        tweetsList.add(tweets)
     }
 }
--- app/src/main/java/sonia/com/flohtweets/adapter/TweetsAdapter.kt
+++ app/src/main/java/sonia/com/flohtweets/adapter/TweetsAdapter.kt
@@ -5,6 +5,8 @@ import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import sonia.com.flohtweets.R
 import sonia.com.flohtweets.model.Tweets
 import kotlinx.android.synthetic.main.layout_tweet.view.*
@@ -24,6 +26,12 @@ class TweetsAdapter(
     override fun onBindViewHolder(p0: TweetsHolder, p1: Int) {
         val tweets = tweetsList[p1]
 
+        Glide.with(context)
+            .load(/*tweets.tweetUserProfile*/context.resources.getDrawable(R.drawable.ic_launcher_background))
+            .into(p0.itemView.tweetUserProfile)
+
+        p0.itemView.tweetUsername.text = tweets.tweetUsername
+        //p0.itemView.tweetMessage.text = tweets.tweetMessage
     }
 
     inner class TweetsHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
