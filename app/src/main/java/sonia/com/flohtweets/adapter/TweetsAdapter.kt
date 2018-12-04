930919985f68006c7770299e2a7e14200586a546
= <=>
2018-12-04 14:50:12 +0530

    Commit 4.2 Added DataBinding. Still giving some issues.

--- app/src/main/java/sonia/com/flohtweets/adapter/TweetsAdapter.kt
+++ app/src/main/java/sonia/com/flohtweets/adapter/TweetsAdapter.kt
@@ -1,6 +1,7 @@
 package sonia.com.flohtweets.adapter
 
 import android.content.Context
+import android.databinding.DataBindingUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -8,6 +9,7 @@ import android.view.ViewGroup
 import com.bumptech.glide.Glide
 import sonia.com.flohtweets.R
 import kotlinx.android.synthetic.main.layout_tweet.view.*
+import sonia.com.flohtweets.databinding.LayoutTweetBinding
 import sonia.com.flohtweets.model.Statuses
 import sonia.com.flohtweets.utils.Constants
 
@@ -20,8 +22,10 @@ class TweetsAdapter(
 
         return when (viewType) {
             Constants.TWEET_ITEM -> {
-                val view = LayoutInflater.from(context).inflate(R.layout.layout_tweet, container, false)
-                TweetsHolder(view)
+                val view = LayoutInflater.from(context)//.inflate(R.layout.layout_tweet, container, false)
+                val tweetBinding: LayoutTweetBinding =
+                    DataBindingUtil.inflate(view, R.layout.layout_tweet, container, false)
+                TweetsHolder(tweetBinding)
             }
             else -> {
                 val view = LayoutInflater.from(context).inflate(R.layout.layout_load_more, container, false)
@@ -40,9 +44,10 @@ class TweetsAdapter(
                     .load(tweets?.twitterUser?.userProfileURL)
                     .into(viewHolder.itemView.tweetUserProfile)
 
-                viewHolder.itemView.tweetUsername.text = tweets?.twitterUser?.name
-                viewHolder.itemView.tweetScreename.text = "@${tweets?.twitterUser?.userName}"
-                viewHolder.itemView.tweetMessage.text = tweets?.tweetMessage
+                viewHolder.bind(tweets)
+                /* viewHolder.itemView.tweetUsername.text = tweets?.twitterUser?.name
+                 viewHolder.itemView.tweetScreename.text = "@${tweets?.twitterUser?.userName}"
+                 viewHolder.itemView.tweetMessage.text = tweets?.tweetMessage*/
             }
             else -> {
             }
@@ -71,7 +76,17 @@ class TweetsAdapter(
         this.tweetsList.addAll(tweetsList)
     }
 
-    inner class TweetsHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+    inner class TweetsHolder(private val tweetBinding: LayoutTweetBinding) :
+        RecyclerView.ViewHolder(tweetBinding.root) {
+
+        fun bind(tweet: Statuses?) {
+            with(tweetBinding) {
+                tweetUsername.text = tweet?.twitterUser?.name
+                tweetScreename.text = "@${tweet?.twitterUser?.userName}"
+                tweetMessage.text = tweet?.tweetMessage
+            }
+        }
+    }
 
     inner class LoadMoreHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
 }
--- app/src/main/java/sonia/com/flohtweets/model/Statuses.kt
+++ app/src/main/java/sonia/com/flohtweets/model/Statuses.kt
@@ -1,9 +1,11 @@
 package sonia.com.flohtweets.model
 
+import android.databinding.BaseObservable
 import com.google.gson.annotations.SerializedName
 
 data class Statuses(
     @SerializedName("text")
     val tweetMessage: String,
     @SerializedName("user")
-    val twitterUser: TwitterUser)
+    val twitterUser: TwitterUser
+) : BaseObservable()
