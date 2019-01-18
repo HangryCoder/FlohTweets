c33747eee308c52cae45781f56edab3e01ba65a7
= <=>
2019-01-18 16:42:41 +0530

    Started with Presenter

--- app/src/main/java/sonia/com/flohtweets/flohtweets/FlohTweetsPresenter.kt
+++ app/src/main/java/sonia/com/flohtweets/flohtweets/FlohTweetsPresenter.kt
@@ -1,16 +1,46 @@
 package sonia.com.flohtweets.flohtweets
 
+import sonia.com.flohtweets.model.TwitterAPIResponse
+
 class FlohTweetsPresenter(
     private var flohTweetView: FlohContract.FlohView<FlohTweetsPresenter>?,
     private val flohTweetsRepository: FlohTweetsRepository
 ) : FlohContract.FlohTweetsPresenter {
 
     override fun pullToRefresh() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        flohTweetsRepository.getFlohTweets(object : FlohContract.FlohTweets.GetFlohTweetsCallback {
+            override fun onSuccess(twitterAPIResponse: TwitterAPIResponse) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onError(error: Throwable) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onTerminate() {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+        })
     }
 
     override fun endlessScrolling() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        flohTweetsRepository.loadMoreFlohTweets(
+            remainingUrl = "",
+            listener = object : FlohContract.FlohTweets.LoadMoreFlohTweetsCallback {
+                override fun onSuccess(twitterAPIResponse: TwitterAPIResponse) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onError(error: Throwable) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+                override fun onTerminate() {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+
+            })
     }
 
     override fun onDestroy() {
